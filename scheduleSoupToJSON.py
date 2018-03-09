a6350ceb36b551aff1a5741b75a7adb0d43c1de8
Mack Stanley <mlgprohacker@gmail.com>
2018-03-09 15:26:29 -0600

    add changes to linux branch

--- scheduleSoupToJSON.py
+++ scheduleSoupToJSON.py
@@ -5,20 +5,14 @@ import json
 def makeJSON(): # get soup and dump it to a JSON file
     soup = getScheduleSoup()
     dates = soup.find("div",{"class":"dates"})
-    firstrow = dates.find("ul",{"class":"days row odd max first-child week1"})
-    blocks = firstrow.find_all("li")
+    try:
+        firstrow = dates.find("ul",{"class":"days row even alert min last-child week3 lastweek "})
+        blocks = firstrow.find_all("li")
+    except AttributeError:
+        firstrow = dates.find("ul",{"class":"days row even min last-child week3 lastweek"})
+        blocks = firstrow.find_all("li")
     blocks.remove(blocks[len(blocks)-1])
     mydict = {}
-    for block in blocks:
-        date = block.div.span.text
-        try:
-            hours = block.find("span",{"class":"hours"}).text
-            mydict[date] = hours
-        except:
-            mydict[date] = "off"
-    secondrow = dates.find("ul",{"class":"days row even min last-child week2 lastweek"})
-    blocks = secondrow.find_all("li")
-    blocks.remove(blocks[len(blocks)-1])
     for block in blocks:
         date = block.div.span.text
         try:
